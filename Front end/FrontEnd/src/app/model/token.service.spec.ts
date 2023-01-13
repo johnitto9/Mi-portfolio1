d141736f12a50905722890638df8c1d9890852ec
johnitto9 <109311731+johnitto9@users.noreply.github.com>
2023-01-13 00:56:43 -0300

    Integración JWT y interceptor

--- Front end/FrontEnd/src/app/model/token.service.spec.ts
+++ Front end/FrontEnd/src/app/model/token.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { TokenService } from '../service/token.service';
 
-import { PersonaService } from './persona.service';
 
-describe('PersonaService', () => {
-  let service: PersonaService;
+describe('TokenService', () => {
+  let service: TokenService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(PersonaService);
+    service = TestBed.inject(TokenService);
   });
 
   it('should be created', () => {
